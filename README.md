aa12dcc42054e7e083ff3e3da39aa492c2b3332f
Noah Pendleton <noahp@noreply.github.com>
2020-09-30 20:16:32 -0400

    update

--- README.md
+++ README.md
@@ -1,10 +1,8 @@
-[![GitHub](https://img.shields.io/badge/GitHub-noahp/gh-pr-update-description-8da0cb?style=for-the-badge&logo=github)](https://github.com/noahp/gh-pr-update-description)
+[![GitHub](https://img.shields.io/badge/GitHub-noahp%2Fgh--pr--update--description-8da0cb?style=for-the-badge&logo=github)](https://github.com/noahp/gh-pr-update-description)
 [![PyPI
 version](https://img.shields.io/pypi/v/gh-pr-update-description.svg?style=for-the-badge)](https://pypi.org/project/gh-pr-update-description/)
 [![PyPI
 pyversions](https://img.shields.io/pypi/pyversions/gh-pr-update-description.svg?style=for-the-badge)](https://pypi.python.org/pypi/gh-pr-update-description/)
-[![License:
-MIT](https://img.shields.io/badge/License-MIT-brightgreen.svg?style=for-the-badge)](https://opensource.org/licenses/MIT)
 
 # 🔃 GitHub PR Update Description
 
--- setup.py
+++ setup.py
@@ -14,11 +14,9 @@ with io.open("README.md", "rt", encoding="utf8") as readmefile:
     README = readmefile.read()
 
 setup(
-    # I think using `-` instead of `_` is more user-friendly, but due to python
-    # import directives not allowing `-`, keep everything consistent with `_`.
-    name="emoji-fzf",
-    version="0.4.0",
-    description="Emoji searcher for use with fzf",
+    name="gh-pr-update-description",
+    version="0.1.0",
+    description="🔃 Update Github PR description with top commit title + body",
     author="Noah Pendleton",
     author_email="2538614+noahp@users.noreply.github.com",
     url="https://github.com/noahp/gh-pr-update-description",
@@ -28,15 +26,16 @@ setup(
     },
     long_description=README,
     long_description_content_type="text/markdown",
-    install_requires=["click"],
+    install_requires=["click", "gitpython", "pygithub"],
     # using markdown as pypi description:
     # https://dustingram.com/articles/2018/03/16/markdown-descriptions-on-pypi
     setup_requires=["setuptools>=38.6.0", "wheel>=0.31.0", "twine>=1.11.0"],
     modules=["gh_pr_update_description"],
-    entry_points={"console_scripts": ["gh-pr-update-description = gh_pr_update_description:main"]},
+    entry_points={
+        "console_scripts": ["gh-pr-update-description = gh_pr_update_description:main"]
+    },
     classifiers=[
         "License :: OSI Approved :: MIT License",
-        "Programming Language :: Python :: 2.7",
         "Programming Language :: Python :: 3.6",
         "Programming Language :: Python :: 3.7",
         "Programming Language :: Python :: 3.8",
